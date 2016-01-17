7fbbe45b3c42003e2396b05b9f11692b27f38fdb
Philipp Neubauer <neubauer.phil@gmail.com>
2016-01-18 11:42:37 +1300

    package data

---
+++ TaxMeta/R/sizes.R
@@ -0,0 +1,12 @@
+#' Mammal sizes mammals
+#'
+#' A dataset containing mammal sizes.
+#'
+#' \itemize{
+#'   \item lmass log10 body mass
+#'   \item cmass mass in gramm
+#' }
+#'
+#' @format A data frame with 4,316 rows and 9 variables
+#' @source \url{http://esapubs.org/archive/ecol/E089/051/metadata.htm}
+"sizes"
